be11c3ee280384be5ee6ed24e3f29ec31b707f8c
Ayan777-create <79051656+Ayan777-create@users.noreply.github.com>
2021-02-15 00:00:56 +0530

    Update __main__.py

--- tg_bot/__main__.py
+++ tg_bot/__main__.py
@@ -19,14 +19,14 @@ from tg_bot.modules.helper_funcs.chat_status import is_user_admin
 from tg_bot.modules.helper_funcs.misc import paginate_modules
 
 PM_START_TEXT = """
-Hi {}, my name is {}! If you have any questions on how to use me, read /help - and then head to @MarieSupport.
+Hi {}, my name is {Eren Yeager}! If you have any questions on how to use me, read /help
 
-I'm a group manager bot built in python3, using the python-telegram-bot library, and am fully opensource; \
-you can find what makes me tick [here](github.com/PaulSonOfLars/tgbot)!
+I'm a group manager bot built in python3, help you to protect your group from titans,help me to grow my Eldian Empire.
 
-Feel free to submit pull requests on github, or to contact my support group, @MarieSupport, with any bugs, questions \
-or feature requests you might have :)
-I also have a news channel, @MarieNews for announcements on new features, downtime, etc.
+
+
+
+I also have a anime subbing group, @hindisubanimes for subbed mangas a d anime.
 
 You can find the list of available commands with /help.
 
@@ -34,7 +34,7 @@ If you're enjoying using me, and/or would like to help me survive in the wild, h
 """
 
 HELP_STRINGS = """
-Hey there! My name is *{}*.
+Hey there! My name is *{Eren Yeager}*.
 I'm a modular group management bot with a few fun extras! Have a look at the following for an idea of some of \
 the things I can help you with.
 
